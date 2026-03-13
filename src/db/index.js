5b5a612fcbc1f1cbc88790ab54c2cb76dad5e8ac
Satish <satishbhardwaj8288@gmail.com>
2026-03-13 22:43:58 +0530

    db connection partially implemented

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(
+      `\n MongoDB connected \n DB HOST :${connectionInstance.connection.host}`
+    );
+    // console.log(Object.keys(connectionInstance.connection));
+  } catch (error) {
+    console.log("MONGODB connection FAILED: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
