cdb0fe8ec912fa429c4d553ba954a60b166b960b
Satish <satishbhardwaj8288@gmail.com>
2026-03-23 21:29:14 +0530

    logout added & refcator register and login

--- src/routes/user.route.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { userRegister } from "../controllers/user.controller";
-const router = Router();
-
-router.route("/register").post(userRegister);
-
-export default router;
